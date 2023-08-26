ae36527778fcb05154118a2e15f9c6f557f2a6c5
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-26 14:26:15 -0400

    added see data into seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO departments (name) VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Marketing'),
+  ('Finance');
+
+INSERT INTO roles (title, salary, department_id) VALUES
+  ('Sales Manager', 80000, 1),
+  ('Software Engineer', 90000, 2),
+  ('Marketing Coordinator', 60000, 3),
+  ('Financial Analyst', 75000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+  ('John', 'Doe', 1, NULL),
+  ('Jane', 'Smith', 2, 1),
+  ('Michael', 'Johnson', 3, 1),
+  ('Emily', 'Williams', 4, NULL);

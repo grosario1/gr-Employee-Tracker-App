94269cfc5207a65f325575eecf7e703273f803df
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-26 13:15:59 -0400

    updated connection code in server.js

--- server.js
+++ server.js
@@ -12,8 +12,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const dbConfig = {
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
+
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+
+    query(sql, params = [], callback) {
+        this.connection.query(sql, params, (err, result) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return callback(err);
+            }
+            callback(null, result);
+        });
+    }
+
+    close() {
+        this.connection.end();
+    }
+}
+
+const connection = new Database(dbConfig);
+

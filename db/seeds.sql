324ae4926ac84bd9c394bda8a170f9c653926122
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-26 16:02:40 -0400

    updated seeds data

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,10 +8,12 @@ INSERT INTO roles (title, salary, department_id) VALUES
   ('Sales Manager', 80000, 1),
   ('Software Engineer', 90000, 2),
   ('Marketing Coordinator', 60000, 3),
-  ('Financial Analyst', 75000, 4);
+  ('Financial Analyst', 75000, 4),
+  ('Software Engineering Manager', 135000, 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
   ('John', 'Doe', 1, NULL),
   ('Jane', 'Smith', 2, 1),
   ('Michael', 'Johnson', 3, 1),
-  ('Emily', 'Williams', 4, NULL);
+  ('Emily', 'Williams', 4, NULL),
+  ('Robert', 'Miles', 2, 5);

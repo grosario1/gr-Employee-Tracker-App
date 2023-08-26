a9c86dce9977d7f384c019688c482bd9abf1bf14
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-26 13:51:22 -0400

    updated code in server.js

--- server.js
+++ server.js
@@ -79,9 +79,21 @@ class Department {
     }
   }
 
+  class Employee {
+    constructor(db) {
+      this.db = db;
+    }
+  
+    getAllEmployees(callback) {
+      const sql = 'SELECT e.id, e.first_name, e.last_name, r.title AS role, d.name AS department, r.salary, CONCAT(m.first_name, " ", m.last_name) AS manager FROM employees e LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN departments d ON r.department_id = d.id LEFT JOIN employees m ON e.manager_id = m.id';
+      this.db.query(sql, [], callback);
+    }
+  }
+
 function startApp() {
   const department = new Department(connection);
   const role = new Role(connection);
+  const employee = new Employee(connection);
 
   const actions = {
     'View all departments': () => {

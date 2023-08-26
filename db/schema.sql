6bbec09fed93282f12827b5e0791c7ee4f6e6a94
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-08-26 14:05:42 -0400

    added insert logic into the server.js

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE departments (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
